27481d1b72fb6bbfc331522c99b09b13f75287b2
Gareth James <gareth.james@pureclarity.com>
2019-03-14 12:20:17 +0000

    Added command classes to allow running feeds from the command lines

---
+++ Console/Command/RunFeed/All.php
@@ -0,0 +1,87 @@
+<?php
+namespace Pureclarity\Core\Console\Command\RunFeed;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Pureclarity\Core\Model\Feed;
+use Pureclarity\Core\Model\CronFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\State;
+
+class All extends Command
+{
+    const RUN_FEEDS = [
+        Feed::FEED_TYPE_PRODUCT,
+        Feed::FEED_TYPE_CATEGORY,
+        Feed::FEED_TYPE_BRAND,
+        Feed::FEED_TYPE_USER,
+    ];
+    
+    /** @var \Pureclarity\Core\Model\CronFactory */
+    private $feedRunnerFactory;
+    
+    /** @var \Magento\Framework\App\State */
+    private $state;
+    
+    /** @var \Magento\Store\Model\StoreManagerInterface */
+    private $storeManager;
+    
+    /**
+     * @param \Pureclarity\Core\Model\CronFactory $feedRunner
+     * @param \Magento\Framework\App\State $state
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param string|null $name
+     */
+    public function __construct(
+        CronFactory $feedRunnerFactory,
+        State $state,
+        StoreManagerInterface $storeManager,
+        $name = null
+    ) {
+        $this->feedRunnerFactory = $feedRunnerFactory;
+        $this->state             = $state;
+        $this->storeManager      = $storeManager;
+        parent::__construct($name);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        $this->setName('pureclarity:runfeed:all')
+             ->setDescription('Run All Data Feeds (Product, Category, Brands, Users)');
+            
+        parent::configure();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        try {
+            $this->state->setAreaCode('adminhtml');
+        } catch (\Exception $e) {
+            $output->writeln($e->getMessage());
+        }
+        
+        /** @var \Pureclarity\Core\Model\Cron */
+        $feedRunner = $this->feedRunnerFactory->create();
+        
+        foreach ($this->storeManager->getWebsites() as $website) {
+            foreach ($website->getGroups() as $group) {
+                $stores = $group->getStores();
+                foreach ($stores as $store) {
+                    foreach (self::RUN_FEEDS as $feedType) {
+                        $output->writeln('Running ' . ucfirst($feedType) . ' Feed for Store ID ' . $store->getId());
+                        $feedRunner->selectedFeeds($store->getId(), [$feedType]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Console/Command/RunFeed/Product.php
@@ -0,0 +1,80 @@
+<?php
+namespace Pureclarity\Core\Console\Command\RunFeed;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Pureclarity\Core\Model\Feed;
+use Pureclarity\Core\Model\CronFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\State;
+
+class Product extends Command
+{
+    /** @var \Pureclarity\Core\Model\CronFactory */
+    private $feedRunnerFactory;
+    
+    /** @var \Magento\Framework\App\State */
+    private $state;
+    
+    /** @var \Magento\Store\Model\StoreManagerInterface */
+    private $storeManager;
+    
+    /**
+     * @param \Pureclarity\Core\Model\CronFactory $feedRunner
+     * @param \Magento\Framework\App\State $state
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param string|null $name
+     */
+    public function __construct(
+        CronFactory $feedRunnerFactory,
+        State $state,
+        StoreManagerInterface $storeManager,
+        $name = null
+    ) {
+        $this->feedRunnerFactory = $feedRunnerFactory;
+        $this->state             = $state;
+        $this->storeManager      = $storeManager;
+        parent::__construct($name);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        $this->setName('pureclarity:runfeed:product')
+             ->setDescription('Run Product Data Feed');
+            
+        parent::configure();
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        try {
+            $this->state->setAreaCode('adminhtml');
+        } catch (\Exception $e) {
+            $output->writeln($e->getMessage());
+        }
+        
+        /** @var \Pureclarity\Core\Model\Cron */
+        $feedRunner = $this->feedRunnerFactory->create();
+        
+        foreach ($this->storeManager->getWebsites() as $website) {
+            foreach ($website->getGroups() as $group) {
+                $stores = $group->getStores();
+                foreach ($stores as $store) {
+                    $output->writeln('Running Product Feed for Store ID ' . $store->getId());
+                    $feedRunner->selectedFeeds($store->getId(), [Feed::FEED_TYPE_PRODUCT]);
+                }
+            }
+        }
+        $mem_usage = memory_get_usage();
+        $mem_peak = memory_get_peak_usage();
+    }
+}
