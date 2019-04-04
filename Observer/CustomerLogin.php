f332707318e9a4194e2829c8f2a04e5015554ad2
Gareth James <gareth.james@pureclarity.com>
2019-04-04 16:22:57 +0100

    Added active check to prevent observer triggering when PureClarity isn't activ

--- Observer/CustomerLogin.php
+++ Observer/CustomerLogin.php
@@ -9,19 +9,26 @@ namespace Pureclarity\Core\Observer;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Customer\Model\Session;
+use Pureclarity\Core\Helper\Data;
 
 class CustomerLogin implements ObserverInterface
 {
     /** @var \Magento\Customer\Model\Session */
     private $customerSession;
+    
+    /** @var \Pureclarity\Core\Helper\Data */
+    private $coreHelper;
 
     /**
      * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Customer\Model\Data $coreHelper
      */
     public function __construct(
-        Session $customerSession
+        Session $customerSession,
+        Data $coreHelper
     ) {
         $this->customerSession = $customerSession;
+        $this->coreHelper      = $coreHelper;
     }
 
     /**
@@ -30,6 +37,8 @@ class CustomerLogin implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        $this->customerSession->setPureclarityTriggerCustomerDetails(true);
+        if ($this->coreHelper->isActive($this->coreHelper->getStoreId())) {
+            $this->customerSession->setPureclarityTriggerCustomerDetails(true);
+        }
     }
 }
