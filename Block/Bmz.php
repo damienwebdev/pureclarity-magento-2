9941032cbbc737acb97a4e316932845bb1d91957
Gareth James <gareth.james@pureclarity.com>
2020-02-26 16:45:24 +0000

    updated Zone widget Block class to remove old dependencies and tidy up code a little

--- Block/Bmz.php
+++ Block/Bmz.php
@@ -12,19 +12,18 @@ use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Widget\Block\BlockInterface;
-use Pureclarity\Core\Helper\Data;
 use Pureclarity\Core\Model\Config\Source\Mode;
 use Pureclarity\Core\Model\CoreConfig;
 
 /**
  * Class Bmz
  *
- * Block for BMZ widgets
+ * Block for Zone widgets
  */
 class Bmz extends Template implements BlockInterface
 {
     /** @var string $_template */
-    protected $_template = "bmz.phtml";
+    protected $_template = 'bmz.phtml';
 
     /** @var integer $storeId */
     private $storeId;
@@ -42,9 +41,7 @@ class Bmz extends Template implements BlockInterface
     private $classes;
 
     /** @var string $bmzData */
-    private $bmzData = "";
-
-    private $coreHelper;
+    private $bmzData = '';
 
     /** @var Registry $registry */
     private $registry;
@@ -57,7 +54,6 @@ class Bmz extends Template implements BlockInterface
 
     /**
      * @param Context $context
-     * @param Data $coreHelper
      * @param Registry $registry
      * @param BlockFactory $cmsBlockFactory
      * @param CoreConfig $coreConfig
@@ -65,13 +61,11 @@ class Bmz extends Template implements BlockInterface
      */
     public function __construct(
         Context $context,
-        Data $coreHelper,
         Registry $registry,
         BlockFactory $cmsBlockFactory,
         CoreConfig $coreConfig,
         array $data = []
     ) {
-        $this->coreHelper      = $coreHelper;
         $this->registry        = $registry;
         $this->cmsBlockFactory = $cmsBlockFactory;
         $this->coreConfig      = $coreConfig;
@@ -80,26 +74,36 @@ class Bmz extends Template implements BlockInterface
             $data
         );
     }
-    
+
+    /**
+     * Adds the given field & value to the current Zone data
+     *
+     * @param string $field
+     * @param string $value
+     */
     public function addBmzData($field, $value)
     {
-        $this->bmzData = $this->bmzData . $field . ':' . $value . ';';
+        $this->bmzData .= $field . ':' . $value . ';';
     }
 
+    /**
+     * @return Template|void
+     * @throws NoSuchEntityException
+     */
     public function _beforeToHtml()
     {
         // Get some parameters
         $this->debug = $this->coreConfig->isZoneDebugActive($this->getStoreId());
         $this->bmzId = $this->escapeHtml($this->getData('bmz_id'));
 
-        if ($this->bmzId == null || $this->bmzId == "") {
-            $this->_logger->error("PureClarity: Zone block instantiated without a Zone Id.");
+        if ($this->bmzId === null || $this->bmzId === '') {
+            $this->_logger->error('PureClarity: Zone block instantiated without a Zone Id.');
         } else {
             $this->addBmzData('bmz', $this->bmzId);
 
             // Set product data
-            $product = $this->registry->registry("product");
-            if ($product != null) {
+            $product = $this->registry->registry('product');
+            if ($product !== null) {
                 $this->addBmzData('sku', $product->getId());
             }
 
@@ -113,7 +117,7 @@ class Bmz extends Template implements BlockInterface
         // Generate debug text if needed
         $debugContent = '';
         if ($this->debug) {
-            $debugContent = "<p>PureClarity Zone: $this->bmzId</p>";
+            $debugContent = '<p>PureClarity Zone: ' . $this->bmzId . '</p>';
         }
 
         // Get the fallback content
@@ -126,7 +130,7 @@ class Bmz extends Template implements BlockInterface
         if ($fallbackBlock && $fallbackBlock->getIsActive()) {
             $fallbackContent = $fallbackBlock->getContent();
             if ($this->debug) {
-                $debugContent .= "<p>Fallback block: $fallbackCmsBlock.</p>";
+                $debugContent .= '<p>Fallback block: ' . $fallbackCmsBlock . '.</p>';
             }
         }
 
@@ -137,7 +141,7 @@ class Bmz extends Template implements BlockInterface
         // Get a list of the custom classes for this BMZs div tag
         $customClasses = $this->getData('pc_bmz_classes');
         if ($customClasses) {
-            $allClasses = explode(",", $customClasses);
+            $allClasses = explode(',', $customClasses);
         } else {
             $allClasses = [];
         }
@@ -146,47 +150,71 @@ class Bmz extends Template implements BlockInterface
         $displayMode = $this->getData('pc_bmz_display_mode');
 
         // Add more classes to the class list where they are needed to identify desktop-specific or mobile-specific BMZs
-        if ($displayMode == "mobile") {
-            $allClasses[] = "pureclarity_magento_mobile";
-        } elseif ($displayMode == "desktop") {
-            $allClasses[] = "pureclarity_magento_desktop";
+        if ($displayMode === 'mobile') {
+            $allClasses[] = 'pureclarity_magento_mobile';
+        } elseif ($displayMode === 'desktop') {
+            $allClasses[] = 'pureclarity_magento_desktop';
         }
 
         $applyBuffer = $this->getData('pc_bmz_buffer');
-        if ($applyBuffer == 1 || $applyBuffer == "true") {
-            $allClasses[] = "pc_bmz_buffer";
+        if ($applyBuffer === 1 || $applyBuffer === 'true') {
+            $allClasses[] = 'pc_bmz_buffer';
         }
 
         // Content is now final
         $this->content = $content;
 
         // Classes are now final
-        $allClasses[] = "pc_bmz";
-        $allClassesStr = implode(" ", $allClasses);
+        $allClasses[] = 'pc_bmz';
+        $allClassesStr = implode(' ', $allClasses);
         $allClassesStr = $this->escapeHtml($allClassesStr);
         $this->classes = $allClassesStr;
     }
 
+    /**
+     * Gets whether this Zone should be in debug mode
+     *
+     * @return bool
+     */
     public function getDebugMode()
     {
         return $this->debug;
     }
 
+    /**
+     * Gets the id of this Zone
+     *
+     * @return string
+     */
     public function getBmzId()
     {
         return $this->bmzId;
     }
 
+    /**
+     * Returns the html to be used for this Zone
+     *
+     * @return string
+     */
     public function getContentHtml()
     {
         return $this->content;
     }
 
+    /**
+     * Returns any classes to be used with this Zone
+     * @return string
+     */
     public function getClasses()
     {
         return $this->classes;
     }
 
+    /**
+     * Returns the data attribute for this zone
+     *
+     * @return string
+     */
     public function getBmzDataHtml()
     {
         $data = 'data-pureclarity="' . $this->bmzData . '"';
