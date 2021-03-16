cf5126ab46865da3c64d8c9dd4e82bf3f0766051
Gareth James <gareth.james@pureclarity.com>
2021-03-16 20:17:29 +0000

    Updating delta cron job to use new delta class

--- Test/Unit/Cron/RunDeltasTest.php
+++ Test/Unit/Cron/RunDeltasTest.php
@@ -7,10 +7,13 @@ declare(strict_types=1);
 
 namespace Pureclarity\Core\Test\Unit\Cron;
 
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
 use Pureclarity\Core\Cron\RunDeltas;
-use Pureclarity\Core\Model\Cron;
+use Pureclarity\Core\Model\CoreConfig;
+use Pureclarity\Core\Model\Delta\Runner;
 
 /**
  * Class RunDeltasTest
@@ -22,38 +25,145 @@ class RunDeltasTest extends TestCase
     /** @var MockObject|RunDeltas $object */
     private $object;
 
-    /** @var MockObject|Cron $cron */
-    private $cron;
+    /** @var MockObject|Runner $cron */
+    private $runner;
+
+    /** @var StoreManagerInterface|MockObject */
+    private $storeManager;
+
+    /** @var MockObject|CoreConfig */
+    private $coreConfig;
 
     /**
      * Sets up RunDeltas with dependencies
      */
-    protected function setUp()
+    protected function setUp() : void
     {
-        $this->cron = $this->getMockBuilder(Cron::class)
+        $this->storeManager = $this->getMockBuilder(StoreManagerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->coreConfig = $this->getMockBuilder(CoreConfig::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->runner = $this->getMockBuilder(Runner::class)
             ->disableOriginalConstructor()
             ->getMock();
 
         $this->object = new RunDeltas(
-            $this->cron
+            $this->storeManager,
+            $this->coreConfig,
+            $this->runner
         );
+    }
+
+    /**
+     * @param int[] $storeIds
+     */
+    private function setupStoreData(array $storeIds): void
+    {
+        $stores = [];
+        $coreConfigIndex = 0;
+        $runnerIndex = 0;
+        foreach ($storeIds as $storeId => $deltasEnabled) {
+            $store = $this->getMockBuilder(StoreInterface::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+
+            $store->method('getId')
+                ->willReturn($storeId);
+
+            $stores[$storeId] = $store;
+
+            $this->coreConfig->expects(self::at($coreConfigIndex))
+                ->method('areDeltasEnabled')
+                ->with($storeId)
+                ->willReturn($deltasEnabled);
+
+            if ($deltasEnabled) {
+                $this->runner->expects(self::at($runnerIndex))
+                    ->method('runDeltas')
+                    ->with($storeId);
+                $runnerIndex++;
+            }
+            $coreConfigIndex++;
+        }
+
+        if ($runnerIndex === 0) {
+            $this->runner->expects(self::never())
+                ->method('runDeltas');
+        }
+
+        $this->storeManager->expects(self::at(0))
+            ->method('getStores')
+            ->willReturn($stores);
     }
 
     /**
      * Tests class gets instantiated correctly
      */
-    public function testInstance()
+    public function testInstance(): void
     {
         self::assertInstanceOf(RunDeltas::class, $this->object);
     }
 
     /**
-     * Tests how execute handles a single store setup with no signup request present
+     * Tests how execute handles a single store setup with deltas disabled
      */
-    public function testExecute()
+    public function testSingleStoreNotEnabled(): void
     {
-        $this->cron->expects(self::once())
-            ->method('reindexData');
+        $this->setupStoreData([1 => false]);
+        $this->object->execute();
+    }
+
+    /**
+     * Tests how execute handles a single store setup with deltas enabled
+     */
+    public function testSingleStoreEnabled(): void
+    {
+        $this->setupStoreData([1 => true]);
+        $this->object->execute();
+    }
+
+    /**
+     * Tests how execute handles a multi store setup with deltas disabled on all
+     */
+    public function testMultiStoreNoneEnabled(): void
+    {
+        $this->setupStoreData([
+            1 => false,
+            17 => false,
+            42 => false
+        ]);
+
+        $this->object->execute();
+    }
+
+    /**
+     * Tests how execute handles a multi store setup with deltas enabled on one store
+     */
+    public function testMultiStoreOneEnabled(): void
+    {
+        $this->setupStoreData([
+            1 => false,
+            17 => true,
+            42 => false
+        ]);
+
+        $this->object->execute();
+    }
+
+    /**
+     * Tests how execute handles a multi store setup with deltas enabled on all stores
+     */
+    public function testMultiStoreAllEnabled(): void
+    {
+        $this->setupStoreData([
+            1 => true,
+            17 => true,
+            42 => true
+        ]);
 
         $this->object->execute();
     }
