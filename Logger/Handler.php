044321e194c810e5f7c4f59c1dc9119a1b68988c
Gareth James <gareth.james@pureclarity.com>
2021-07-27 11:56:46 +0100

    Changed logger to log to a single file and fixed issue with repeated log entries. Also factored in debug logging config

---
+++ Logger/Handler.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+/**
+ * Copyright © PureClarity. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace Pureclarity\Core\Logger;
+
+use Magento\Framework\Filesystem\DriverInterface;
+use Magento\Framework\Logger\Handler\Base as BaseHandler;
+use Monolog\Logger;
+use Pureclarity\Core\Model\CoreConfig;
+
+/**
+ * Class Handler
+ *
+ * Logs messages to a PureClarity log file
+ */
+class Handler extends BaseHandler
+{
+    /**
+     * Minimum logging level
+     *
+     * @var int
+     */
+    protected $loggerType = Logger::DEBUG;
+
+    /**
+     * File name
+     *
+     * @var string
+     */
+    protected $fileName = '/var/log/pureclarity.log';
+
+    /** @var CoreConfig */
+    private $coreConfig;
+
+    /**
+     * @param CoreConfig $coreConfig
+     * @param DriverInterface $filesystem
+     * @param null $filePath
+     * @param null $fileName
+     */
+    public function __construct(
+        CoreConfig $coreConfig,
+        DriverInterface $filesystem,
+        $filePath = null,
+        $fileName = null
+    ) {
+        $this->coreConfig = $coreConfig;
+        parent::__construct($filesystem, $filePath, $fileName);
+    }
+
+    /**
+     * Only writes if it's not a debug log, or debug logging is enabled
+     *
+     * @param $record array
+     *
+     * @return void
+     */
+    public function write(array $record)
+    {
+        if ($record['level'] !== Logger::DEBUG || $this->coreConfig->isDebugLoggingEnabled()) {
+            parent::write($record);
+        }
+    }
+}
