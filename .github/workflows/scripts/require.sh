a5484eb887d099219f91cd3c1e983f9e53a22f36
Gareth James <gareth.james@pureclarity.com>
2021-08-17 16:26:51 +0100

    tweak to require script

--- .github/workflows/scripts/require.sh
+++ .github/workflows/scripts/require.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $MAGENTO_ROOT
-composer require $COMPOSER_NAME:$GITHUB_SHA --no-update --no-interaction
+composer require $COMPOSER_NAME:dev-dev#$GITHUB_SHA --no-update --no-interaction
