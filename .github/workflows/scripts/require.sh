42e9eb7c49b685aeb987dd1d86490933cb8bb2bc
PeterBrooksbank <peter.c.brooksbank@gmail.com>
2022-07-08 12:13:54 +0100

    Update require.sh
    Change to use dispatcher branch

--- .github/workflows/scripts/require.sh
+++ .github/workflows/scripts/require.sh
@@ -2,4 +2,4 @@
 
 # Changes the composer require to use the current commit ref, rather than the last released version
 cd $MAGENTO_ROOT
-composer require $COMPOSER_NAME:dev-dev#$GITHUB_SHA --no-update --no-interaction
+composer require $COMPOSER_NAME:dev-$GITHUB_REF --no-update --no-interaction
